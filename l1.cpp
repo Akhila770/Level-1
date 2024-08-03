676dcd7e3c8b1f40327763fae37015f160564172
Akhila770 <reddyakhila307@gmail.com>
2024-08-03 23:47:35 +0530

    Add files via upload

---
+++ l1.cpp
@@ -0,0 +1,13 @@
+// quetion 1
+//Get a number from user and add 2 to that number and print the result?
+//code:
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a :");
+	scanf("%d",&a);
+	b=a+2;
+	printf("output:%d ",b);
+	
+}
