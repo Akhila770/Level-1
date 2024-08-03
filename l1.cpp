9ed3aad4ed83e444b85fd0b3af959322927ac972
Akhila770 <reddyakhila307@gmail.com>
2024-08-03 23:50:39 +0530

    Update l1.cpp

--- l1.cpp
+++ l1.cpp
@@ -11,3 +11,15 @@ int main()
 	printf("output:%d ",b);
 	
 }
+// level 1 , quetion 2
+//Get a number from user and subtract 5 to that number and print the result?
+//code:
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a :");
+	scanf("%d",&a);
+	b=a-5;
+	printf("output:%d ",b);
+}
